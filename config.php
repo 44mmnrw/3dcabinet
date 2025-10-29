c3d22e8931179e3ec3cf09fabab0097ef6eb27ad
Yuri.R <44mmnrw@gmail.com>
2025-10-29 18:55:59 +0300

    Добавил админку, начал ее разработку.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'db_host' => '127.0.0.1',
+    'db_name' => '3dcabinet',
+    'db_user' => 'admin',
+    'db_pass' => '4bq;=m=)',
+    'debug' => false, // Включение/выключение debug.php
+];
