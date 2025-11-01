cf1e0db9cf2731974ef9c6c88873834bb6618c36
Yuri.R <44mmnrw@gmail.com>
2025-11-01 04:55:40 +0300

    Тени в стилях, иконка чек-лист.

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
     <meta name="description" content="3Cabinet — онлайн-конфигуратор серверного шкафа" />
     <title>3Cabinet — Конфигуратор</title>
 
-    <!-- <link rel="preload" href="fonts/InterVariable.woff2" as="font" type="font/woff2"> -->
+    <link rel="preload" href="fonts/InterVariable.woff2" as="font" type="font/woff2">
     <link rel="preload" href="fonts/InterVariable-Italic.woff2" as="font" type="font/woff2">
 
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/styles.css" />
-    <!-- стиль перенесен в css/styles.css -->
-    </head>
+    <link rel="stylesheet" href="css/styles.css">
+        </head>
 <body class="body">
     <!-- Опционально: инлайн-спрайт, чтобы не грузить внешний файл и улучшить совместимость -->
     <div hidden aria-hidden="true">
@@ -24,13 +23,13 @@
                 <div class="header-logo">   
                   <a href="/" class="logo-hover-left logo-hover-right logo-hover-digits">
                     <svg class="icon-logo-box-left" width="40" height="40" role="img">
-                      <use href="#icon-logo-box-left" xlink:href="#icon-logo-box-left"></use>
+                      <use xlink:href="#icon-logo-box-left"></use>
                     </svg>
                     <svg class="icon-logo-box-right" width="40" height="40" role="img">
-                      <use href="#icon-logo-box-right" xlink:href="#icon-logo-box-right"></use>
+                      <use xlink:href="#icon-logo-box-right"></use>
                     </svg>
                     <svg class="icon-logo-digits" width="70" height="70" role="img">
-                      <use href="#icon-logo-digits" xlink:href="#icon-logo-digits"></use>
+                      <use xlink:href="#icon-logo-digits"></use>
                     </svg>
                     </a>
                 </div>
@@ -49,13 +48,34 @@
             </div>
         </div>    
     </header>
-    <main class="main">
+    <main class="content">
         <div class="container">
-            <h1 class="main-title">Конфигуратор серверного шкафа</h1>
-            <p class="main-description">С помощью 3Cabinet вы можете легко спроектировать свой идеальный серверный шкаф.</p>
-            <button class="button blue-button" type="button">Начать конфигурацию</button>
+             <section class="first-section">
+                <div class="intro">
+                    <div class="intro-header">
+                        <h1 class="main-title">Создайте идеальный шкаф за несколько минут</h1>
+                        <p class="main-description">Интеллектуальная система подбора и конфигурирования шкафного оборудования</p>
+                    </div>
+                    <div class="intro-list">                        
+                        <ul>
+                            <li><svg class="icon-check-list" role="img"><use xlink:href="#icon-check-list"></use></svg>
+                                Автоматический расчет параметров</li>
+                            <li><svg class="icon-check-list" role="img"><use xlink:href="#icon-check-list"></use></svg>
+                                3D-визуализация в реальном времени</li>
+                            <li><svg class="icon-check-list" role="img"><use xlink:href="#icon-check-list"></use></svg>
+                                Готовая документация и спецификации</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="selection">
+                    <h2 class="selection-title">Начнем с основных параметров</h2>
+                </div>
+            </section>
+             <section class="second-section">
+             </section>
+             <section class="third-section">
+             </section>
         </div>
     </main>
-
 </body>
 </html>
